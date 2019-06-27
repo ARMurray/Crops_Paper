d8176c264a3ceff442c50849ea56356b02a0d5b9
moaleck <maeck@live.unc.edu>
2019-06-26 20:04:48 -0400

    Temp is updated. Please run and see if it works for you.

--- 6_tMaxStats.R
+++ 6_tMaxStats.R
@@ -1,6 +1,104 @@
-# temperature stats script based off ppt_stats
+#Here I am. Andrew is a dummy. 
+# temp Stats by county
 
-# This script might be amazing some day
+library(sf)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 
-#This is Montana testing this out for today
-#OneMore
+temp <- read.csv("data/tmax_wide_1981_2017.csv")%>%
+  select(-X)
+
+counties <- st_read("data/countiesusa/cb_2017_us_county_5m.shp")
+
+seCounties <- counties%>%
+  filter(STATEFP %in% c(37,45,13))
+
+seCounties$ID <- paste0("F",seCounties$STATEFP,seCounties$COUNTYFP)
+
+tempFlip <- gather(temp, County, temp, -Date)
+
+tempFlip <- tempFlip %>% separate(Date, sep="-", into = c("year", "month", "day"))
+
+grpCounty <- group_by(tempFlip, County)
+
+#Create Monthly Subsets
+
+maytemp  <- tempFlip %>%
+  filter(month == "5")
+
+juntemp<- tempFlip %>%
+  filter(month == "6")
+
+jultemp <- tempFlip %>%
+  filter(month == "7")
+
+augtemp <- tempFlip %>%
+  filter(month == "8")
+
+septemp <- tempFlip %>%
+  filter(month == "9")
+
+octtemp <- tempFlip %>%
+  filter(month == "10")
+
+# Calculate Daily Mean by County for entire time period
+meantemp <- summarize(grpCounty, meantemp = mean(temp))
+seCountytemp <- merge(seCounties, meantemp, by.x = "ID", by.y="County")
+
+plot <- ggplot(seCountytemp)+
+  geom_sf(aes(fill = meantemp))
+
+plot
+
+#st_write(seCountytemp, "~/Documents/Geog_803/Analysis/dailymean.shp")
+
+# Calculate Extreme temp (top 10%, 5%, 1% of values)
+outData <- data.frame()
+counties <- seCounties$ID
+for(n in 1:length(counties)){
+  county <- tempFlip%>%
+    filter(County == counties[n])
+  quant90 <- quantile(county$temp, .90)
+  quant95 <- quantile(county$temp, .95)
+  quant99 <- quantile(county$temp, .99)
+  newData <- data.frame("County" = county$County[1], "Quant90" = quant90, "Quant95" = quant95,"Quant99" = quant99)
+  outData <- rbind(outData, newData)
+}
+
+write.csv(outData, "~/Documents/Geog_803/Analysis/temp_Extremes.csv")
+
+# Count number of days by year that temp extremes were exceeded
+#This needs to be done for each month
+
+tempMerge <- merge(tempFlip, outData, by="County")
+
+outdf <- data.frame()
+for(n in 1981:2017){
+  data <- tempMerge%>%
+    filter(year == n)
+  days90 <- group_by(data, County)%>%
+    tally(temp>Quant90)
+  days95 <- group_by(data, County)%>%
+    tally(temp>Quant95)
+  days99 <- group_by(data, County)%>%
+    tally(temp>Quant99)
+  seasontotal <- group_by(data, County)%>%
+    tally(temp)
+  output <- data.frame("Year" = data$year[1], "exttempDays90" = days90, "exttempDays95" = days95,"exttempDays99" = days99)
+  outdf <- rbind(outdf, output)
+  
+}
+
+colnames(outdf) <- c("Year","County","NumTempDays90", "County", "NumTempDays95", "County", "NumTempDays99")
+
+write.csv(outdf, "~/Documents/Geog_803/Analysis/Num_temp_Extreme_Days.csv")
+
+
+
+
+#Do we really need this?
+dfSpread <- spread(outdf, County, NumDays)
+write.csv(dfSpread, "~/Documents/Geog_803/Analysis/Num_temp_Extreme_Days_Wide.csv")
+
+
