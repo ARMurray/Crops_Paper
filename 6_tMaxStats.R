404addaba04e6ba064b51fd424ced3c20ba3fa53
moaleck <maeck@live.unc.edu>
2019-06-28 10:54:15 -0400

    Temp Extreme Data by Month

--- 6_tMaxStats.R
+++ 6_tMaxStats.R
@@ -64,39 +64,109 @@ octtemp <- tempFlip %>%
   filter(month == "10")
 
 # Calculate Daily Mean by County for entire time period
-meantemp <- summarize(grpCounty, meantemp = mean(temp))
-seCountytemp <- merge(seCounties, meantemp, by.x = "ID", by.y="County")
+#meantemp <- summarize(grpCounty, meantemp = mean(temp))
+#seCountytemp <- merge(seCounties, meantemp, by.x = "ID", by.y="County")
 
-plot <- ggplot(seCountytemp)+
-  geom_sf(aes(fill = meantemp))
+#plot <- ggplot(seCountytemp)+
+ # geom_sf(aes(fill = meantemp))
 
-plot
+#lot
 
 #st_write(seCountytemp, "~/Documents/Geog_803/Analysis/dailymean.shp")
 
 # Calculate Extreme temp (top 10%, 5%, 1% of values)
-outData <- data.frame()
+mayData <- data.frame()
 counties <- seCounties$ID
 for(n in 1:length(counties)){
-  county <- tempFlip%>%
+  county <- maytemp%>%
     filter(County == counties[n])
   quant90 <- quantile(county$temp, .90)
   quant95 <- quantile(county$temp, .95)
   quant99 <- quantile(county$temp, .99)
   newData <- data.frame("County" = county$County[1], "Quant90" = quant90, "Quant95" = quant95,"Quant99" = quant99)
-  outData <- rbind(outData, newData)
+  mayData <- rbind(mayData, newData)
 }
 
-#write.csv(outData, "data/temp_Extremes.csv")
+junData <- data.frame()
+counties <- seCounties$ID
+for(n in 1:length(counties)){
+  county <- juntemp%>%
+    filter(County == counties[n])
+  quant90 <- quantile(county$temp, .90)
+  quant95 <- quantile(county$temp, .95)
+  quant99 <- quantile(county$temp, .99)
+  newData <- data.frame("County" = county$County[1], "Quant90" = quant90, "Quant95" = quant95,"Quant99" = quant99)
+  junData <- rbind(junData, newData)
+}
+
+julData <- data.frame()
+counties <- seCounties$ID
+for(n in 1:length(counties)){
+  county <- jultemp%>%
+    filter(County == counties[n])
+  quant90 <- quantile(county$temp, .90)
+  quant95 <- quantile(county$temp, .95)
+  quant99 <- quantile(county$temp, .99)
+  newData <- data.frame("County" = county$County[1], "Quant90" = quant90, "Quant95" = quant95,"Quant99" = quant99)
+  julData <- rbind(julData, newData)
+}
+
+augData <- data.frame()
+counties <- seCounties$ID
+for(n in 1:length(counties)){
+  county <- augtemp%>%
+    filter(County == counties[n])
+  quant90 <- quantile(county$temp, .90)
+  quant95 <- quantile(county$temp, .95)
+  quant99 <- quantile(county$temp, .99)
+  newData <- data.frame("County" = county$County[1], "Quant90" = quant90, "Quant95" = quant95,"Quant99" = quant99)
+  augData <- rbind(augData, newData)
+}
+
+sepData <- data.frame()
+counties <- seCounties$ID
+for(n in 1:length(counties)){
+  county <- septemp%>%
+    filter(County == counties[n])
+  quant90 <- quantile(county$temp, .90)
+  quant95 <- quantile(county$temp, .95)
+  quant99 <- quantile(county$temp, .99)
+  newData <- data.frame("County" = county$County[1], "Quant90" = quant90, "Quant95" = quant95,"Quant99" = quant99)
+  sepData <- rbind(sepData, newData)
+}
+
+octData <- data.frame()
+counties <- seCounties$ID
+for(n in 1:length(counties)){
+  county <- octtemp%>%
+    filter(County == counties[n])
+  quant90 <- quantile(county$temp, .90)
+  quant95 <- quantile(county$temp, .95)
+  quant99 <- quantile(county$temp, .99)
+  newData <- data.frame("County" = county$County[1], "Quant90" = quant90, "Quant95" = quant95,"Quant99" = quant99)
+  octData <- rbind(octData, newData)
+}
+
+write.csv(mayData, "data/maytemp_Extremes.csv")
+write.csv(junData, "data/juntemp_Extremes.csv")
+write.csv(julData, "data/jultemp_Extremes.csv")
+write.csv(augData, "data/augtemp_Extremes.csv")
+write.csv(sepData, "data/septemp_Extremes.csv")
+write.csv(octData, "data/octtemp_Extremes.csv")
 
 # Count number of days by year that temp extremes were exceeded
 #This needs to be done for each month
 
-tempMerge <- merge(tempFlip, outData, by="County")
+mayMerge <- merge(maytemp, mayData, by="County")
+junMerge <- merge(juntemp, junData, by="County")
+julMerge <- merge(jultemp, julData, by="County")
+augMerge <- merge(augtemp, augData, by="County")
+sepMerge <- merge(septemp, sepData, by="County")
+octMerge <- merge(octtemp, octData, by="County")
 
-outdf <- data.frame()
+maydf <- data.frame()
 for(n in 1981:2017){
-  data <- tempMerge%>%
+  data <- mayMerge%>%
     filter(year == n)
   days90 <- group_by(data, County)%>%
     tally(temp>Quant90)
@@ -104,16 +174,112 @@ for(n in 1981:2017){
     tally(temp>Quant95)
   days99 <- group_by(data, County)%>%
     tally(temp>Quant99)
-  seasontotal <- group_by(data, County)%>%
-    tally(temp)
   output <- data.frame("Year" = data$year[1], "exttempDays90" = days90, "exttempDays95" = days95,"exttempDays99" = days99)
-  outdf <- rbind(outdf, output)
+  maydf <- rbind(maydf, output)
   
 }
 
-colnames(outdf) <- c("Year","County","NumTempDays90", "County1", "NumTempDays95", "County2", "NumTempDays99")
-c <- merge(outdf,meanMerge, by.x=c("County", "Year"), by.y=c("County", "year"), all=TRUE)
-#write.csv(c, "data/Num_temp_Extreme_Days.csv")
+
+jundf <- data.frame()
+for(n in 1981:2017){
+  data <- junMerge%>%
+    filter(year == n)
+  days90 <- group_by(data, County)%>%
+    tally(temp>Quant90)
+  days95 <- group_by(data, County)%>%
+    tally(temp>Quant95)
+  days99 <- group_by(data, County)%>%
+    tally(temp>Quant99)
+  output <- data.frame("Year" = data$year[1], "exttempDays90" = days90, "exttempDays95" = days95,"exttempDays99" = days99)
+  jundf <- rbind(jundf, output)
+  
+}
+
+juldf <- data.frame()
+for(n in 1981:2017){
+  data <- julMerge%>%
+    filter(year == n)
+  days90 <- group_by(data, County)%>%
+    tally(temp>Quant90)
+  days95 <- group_by(data, County)%>%
+    tally(temp>Quant95)
+  days99 <- group_by(data, County)%>%
+    tally(temp>Quant99)
+  output <- data.frame("Year" = data$year[1], "exttempDays90" = days90, "exttempDays95" = days95,"exttempDays99" = days99)
+  juldf <- rbind(juldf, output)
+  
+}
+
+augdf <- data.frame()
+for(n in 1981:2017){
+  data <- augMerge%>%
+    filter(year == n)
+  days90 <- group_by(data, County)%>%
+    tally(temp>Quant90)
+  days95 <- group_by(data, County)%>%
+    tally(temp>Quant95)
+  days99 <- group_by(data, County)%>%
+    tally(temp>Quant99)
+  output <- data.frame("Year" = data$year[1], "exttempDays90" = days90, "exttempDays95" = days95,"exttempDays99" = days99)
+  augdf <- rbind(augdf, output)
+  
+}
+
+sepdf <- data.frame()
+for(n in 1981:2017){
+  data <- sepMerge%>%
+    filter(year == n)
+  days90 <- group_by(data, County)%>%
+    tally(temp>Quant90)
+  days95 <- group_by(data, County)%>%
+    tally(temp>Quant95)
+  days99 <- group_by(data, County)%>%
+    tally(temp>Quant99)
+  output <- data.frame("Year" = data$year[1], "exttempDays90" = days90, "exttempDays95" = days95,"exttempDays99" = days99)
+  sepdf <- rbind(sepdf, output)
+  
+}
+
+octdf <- data.frame()
+for(n in 1981:2017){
+  data <- octMerge%>%
+    filter(year == n)
+  days90 <- group_by(data, County)%>%
+    tally(temp>Quant90)
+  days95 <- group_by(data, County)%>%
+    tally(temp>Quant95)
+  days99 <- group_by(data, County)%>%
+    tally(temp>Quant99)
+  output <- data.frame("Year" = data$year[1], "exttempDays90" = days90, "exttempDays95" = days95,"exttempDays99" = days99)
+  octdf <- rbind(octdf, output)
+  
+}
+
+colnames(maydf) <- c("Year","County","NumTempDays90", "County1", "NumTempDays95", "County2", "NumTempDays99")
+may <- merge(maydf,meanMerge, by.x=c("County", "Year"), by.y=c("County", "year"), all=TRUE)
+
+colnames(jundf) <- c("Year","County","NumTempDays90", "County1", "NumTempDays95", "County2", "NumTempDays99")
+jun <- merge(jundf,meanMerge, by.x=c("County", "Year"), by.y=c("County", "year"), all=TRUE)
+
+colnames(juldf) <- c("Year","County","NumTempDays90", "County1", "NumTempDays95", "County2", "NumTempDays99")
+jul <- merge(juldf,meanMerge, by.x=c("County", "Year"), by.y=c("County", "year"), all=TRUE)
+
+colnames(augdf) <- c("Year","County","NumTempDays90", "County1", "NumTempDays95", "County2", "NumTempDays99")
+aug <- merge(augdf,meanMerge, by.x=c("County", "Year"), by.y=c("County", "year"), all=TRUE)
+
+colnames(sepdf) <- c("Year","County","NumTempDays90", "County1", "NumTempDays95", "County2", "NumTempDays99")
+sep <- merge(sepdf,meanMerge, by.x=c("County", "Year"), by.y=c("County", "year"), all=TRUE)
+
+colnames(octdf) <- c("Year","County","NumTempDays90", "County1", "NumTempDays95", "County2", "NumTempDays99")
+oct <- merge(octdf,meanMerge, by.x=c("County", "Year"), by.y=c("County", "year"), all=TRUE)
+write.csv(may, "data/Num_maytemp_Extreme_Days.csv")
+write.csv(jun, "data/Num_juntemp_Extreme_Days.csv")
+write.csv(jul, "data/Num_jultemp_Extreme_Days.csv")
+write.csv(aug, "data/Num_augtemp_Extreme_Days.csv")
+write.csv(sep, "data/Num_septemp_Extreme_Days.csv")
+write.csv(oct, "data/Num_octtemp_Extreme_Days.csv")
+
+
 
 #Do we really need this?
 dfSpread <- spread(outdf, County, NumDays)
